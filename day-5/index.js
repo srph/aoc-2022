8fdaba884a78c775d5f108d2558bc1f83c4608ec
srph <seraphipod@gmail.com>
2022-12-30 14:55:34 +0800

    Day 5 part 2

--- day-5/index.js
+++ day-5/index.js
@@ -1,6 +1,8 @@
 const { stacks, moves } = require('./input')
 
 function partOne() {
+  const data = [...stacks]
+
   const movement = moves.map((move) => {
     const [_countString, countValue, _fromString, fromValue, _toString, toValue] = move.split(' ')
     const [count, from, to] = [countValue, fromValue, toValue].map(Number)
@@ -8,11 +10,32 @@ function partOne() {
   })
 
   for (const { count, from, to } of movement) {
-    const values = stacks[from].splice(0, count)
-    stacks[to].splice(0, 0, ...values.reverse())
+    const values = data[from].splice(0, count)
+    data[to].splice(0, 0, ...values.reverse())
   }
 
-  const values = stacks.map((stack) => {
+  const values = data.map((stack) => {
+    return stack[0]
+  })
+
+  return values.join('')
+}
+
+function partTwo() {
+  const data = [...stacks]
+
+  const movement = moves.map((move) => {
+    const [_countString, countValue, _fromString, fromValue, _toString, toValue] = move.split(' ')
+    const [count, from, to] = [countValue, fromValue, toValue].map(Number)
+    return { count, from: from - 1, to: to - 1 }
+  })
+
+  for (const { count, from, to } of movement) {
+    const values = data[from].splice(0, count)
+    data[to].splice(0, 0, ...values)
+  }
+
+  const values = data.map((stack) => {
     return stack[0]
   })
 
@@ -20,3 +43,4 @@ function partOne() {
 }
 
 console.log('Part one ->', partOne())
+console.log('Part two ->', partTwo())
