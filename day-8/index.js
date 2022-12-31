3e15d00767413b83da320d9c473533620062d128
srph <seraphipod@gmail.com>
2022-12-31 09:03:10 +0800

    Day 8 part 2

--- day-8/index.js
+++ day-8/index.js
@@ -83,7 +83,108 @@ function stepOne() {
   return visible
 }
 
-function stepTwo() {}
+function stepTwo() {
+  const data = input.map((row) => {
+    return row.split('').map(Number)
+  })
+
+  const scores = []
+
+  for (const [x, row] of data.entries()) {
+    for (const [y, value] of row.entries()) {
+      const getSceneScoreFromTop = () => {
+        const trees = data
+          .map((tree) => {
+            return tree[y]
+          })
+          .filter((_, i) => {
+            return i < x
+          })
+          .reverse()
+
+        let score = 0
+
+        for (const tree of trees) {
+          score++
+
+          if (tree >= value) {
+            break
+          }
+        }
+
+        return score
+      }
+
+      const getSceneScoreFromLeft = () => {
+        const trees = data[x]
+          .filter((_, i) => {
+            return i < y
+          })
+          .reverse()
+
+        let score = 0
+
+        for (const tree of trees) {
+          score++
+
+          if (tree >= value) {
+            break
+          }
+        }
+
+        return score
+      }
+
+      const getSceneScoreFromRight = () => {
+        const trees = data[x].filter((_, i) => {
+          return i > y
+        })
+
+        let score = 0
+
+        for (const tree of trees) {
+          score++
+
+          if (tree >= value) {
+            break
+          }
+        }
+
+        return score
+      }
+
+      const getSceneScoreFromBottom = () => {
+        const trees = data
+          .map((tree) => {
+            return tree[y]
+          })
+          .filter((_, i) => {
+            return i > x
+          })
+
+        let score = 0
+
+        for (const tree of trees) {
+          score++
+
+          if (tree >= value) {
+            break
+          }
+        }
+
+        return score
+      }
+
+      scores.push(
+        getSceneScoreFromTop() * getSceneScoreFromLeft() * getSceneScoreFromRight() * getSceneScoreFromBottom()
+      )
+    }
+  }
+
+  return scores.sort((a, b) => {
+    return b - a
+  })[0]
+}
 
 console.log('Step one ->', stepOne())
 console.log('Step two ->', stepTwo())
